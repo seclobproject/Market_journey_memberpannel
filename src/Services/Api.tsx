981540d59088e1503d75585d1629dd529925d322
seclobproject <seclobclt@gmail.com>
2024-04-26 18:43:40 +0530

    update base url

--- src/Services/Api.tsx
+++ src/Services/Api.tsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
 // import { Show_Toast } from '../pages/Components/Toast';
 
-export const Base_url = 'http://192.168.29.152:6003';
+// export const Base_url = 'http://192.168.29.152:6003';
 // export const Base_url = 'http://192.168.29.77:6003';
-// export const Base_url = 'https://admin.marketjourney.in';
+export const Base_url = 'https://admin.marketjourney.in';
 
 interface ApiCallResponse {
     status?: number;
